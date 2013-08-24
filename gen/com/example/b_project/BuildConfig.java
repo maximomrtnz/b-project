287d0610cd416b5fc8209a3ceab63d543facf905
Maximo Martinez <maximomrtnz@gmail.com>
2013-08-24 14:55:24 -0300

    Start a new project

---
+++ gen/com/example/b_project/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.b_project;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/b_project/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.b_project;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button button = (Button)findViewById(R.id.button1);
+		button.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		TextView textView = (TextView)findViewById(R.id.textView1);
+		textView.setText("Hola Marco");
+	}
+
+}
